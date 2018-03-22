45ed828d9a9beac05c16648f0f589384e596d2a6
Nacho Coll <ignacio.coll@gmail.com>
2018-03-22 18:50:19 +0100

    minors

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task('compress', function (cb) {
 });
 
 gulp.task('update-readme', function (cb) {
-  gutil.log('The NEW version:' + gutil.env.version);
+  gutil.log('The NEW version: ' + gutil.env.version);
+  gutil.log('The NEW build number: ' + gutil.env.travisbuild);
   pump([
         gulp.src(path.resolve(__dirname, './') + '/README.md'),
         replace(new RegExp('https:\/\/d2m9ia44cpx81c\.cloudfront\.net\/js\/services\/(?:\\d*)\/services\.library\.min\.js', 'g'), 'https://d2m9ia44cpx81c.cloudfront.net/js/services/' + gutil.env.version + '/services.library.min.js'),

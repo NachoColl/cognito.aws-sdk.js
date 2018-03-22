5f41ed8aad626abf447203d13715fd34f4ada46e
Nacho Coll <ignacio.coll@gmail.com>
2018-03-22 19:28:44 +0100

    tests

--- gulpfile.js
+++ gulpfile.js
@@ -26,8 +26,8 @@ gulp.task('update-readme', function (cb) {
   gutil.log('The NEW build number: ' + gutil.env.travisbuild);
   pump([
         gulp.src(path.resolve(__dirname, './') + '/README.md'),
-        replace(new RegExp('https:\/\/d2m9ia44cpx81c\.cloudfront\.net\/js\/services\/(\\d*)\/services\.library\.min\.js', 'g'), 'https://d2m9ia44cpx81c.cloudfront.net/js/services/' + gutil.env.version + '/services.library.min.js'),
-        replace(new RegExp('https:\/\/d2m9ia44cpx81c\.cloudfront\.net\/js\/services\/(\\d*)\/services\.aws-sdk\.min\.js', 'g'), 'https://d2m9ia44cpx81c.cloudfront.net/js/services/' + gutil.env.version + '/services.aws-sdk.min.js'),
+        replace(new RegExp('https:\/\/d2m9ia44cpx81c\.cloudfront\.net\/js\/services\/(?:\\d*\\.*)\/services\.library\.min\.js', 'g'), 'https://d2m9ia44cpx81c.cloudfront.net/js/services/' + gutil.env.version + '/services.library.min.js'),
+        replace(new RegExp('https:\/\/d2m9ia44cpx81c\.cloudfront\.net\/js\/services\/(?:\\d*\\.*)\/services\.aws-sdk\.min\.js', 'g'), 'https://d2m9ia44cpx81c.cloudfront.net/js/services/' + gutil.env.version + '/services.aws-sdk.min.js'),
         replace(new RegExp('build number: (?:\\d*)', 'g'), 'build number: ' + gutil.env.travisbuild),
         gulp.dest(path.resolve(__dirname, './'))
     ],
